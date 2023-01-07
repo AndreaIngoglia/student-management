5c64d8169683d060dd5182dc14af2530d66d99c1
andrea <andreaingoglia6@gmail.com>
2023-01-07 15:51:03 +0100

    spring initilizr added

---
+++ applicazione/src/main/java/generatorevoti/GeneratoreVotiApplication.java
@@ -0,0 +1,13 @@
+package generatorevoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratoreVotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratoreVotiApplication.class, args);
+	}
+
+}
---
+++ applicazione/src/test/java/generatorevoti/GeneratoreVotiApplicationTests.java
@@ -0,0 +1,13 @@
+package generatorevoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratoreVotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

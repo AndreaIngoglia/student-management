74d57d6a0c6ddb1f96a602c8b5a5e97ca6c6ed31
a.ingoglia <andreaingoglia6@gmail.com>
2023-01-08 13:01:22 +0100

    added index.html from where send all the requests

--- applicazione/src/main/java/generatorevoti/controllers/ViewsController.java
+++ applicazione/src/main/java/generatorevoti/controllers/ViewsController.java
@@ -2,22 +2,30 @@ package generatorevoti.controllers;
 
 import generatorevoti.services.StudentService;
 import generatorevoti.utils.ValutationInformation;
+import generatorevoti.utils.ValutationPanelInformation;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
 public class ViewsController {
     private StudentService studentService;
-    @GetMapping(path = {"/table"})
-    public String  getValutationInputPanel(@RequestParam String subject, @RequestParam String date, Model model) {
-        model.addAttribute("userForm", new ValutationInformation());
-        model.addAttribute("subject", subject);
-        model.addAttribute("date", date);
-        model.addAttribute("students", studentService.findAll());
+
+    @GetMapping(path = {"/"})
+    public String getMainPage(Model model){
+        model.addAttribute("userForm" , new ValutationPanelInformation());
         return "index";
+    }
+    @GetMapping(path = {"/valutationpanel"})
+    public String  getValutationInputPanel(@ModelAttribute ValutationPanelInformation panelInformation, Model model) {
+        model.addAttribute("userForm", new ValutationInformation());
+        model.addAttribute("subject", panelInformation.getSubject());
+        model.addAttribute("date", panelInformation.getDate());
+        model.addAttribute("students", studentService.findAll());
+        return "valutationpanel";
     }
 }
---
+++ applicazione/src/main/java/generatorevoti/utils/ValutationPanelInformation.java
@@ -0,0 +1,17 @@
+package generatorevoti.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ValutationPanelInformation {
+    private String subject;
+    private String date;
+    private String clazz;
+    private String academicYear;
+}
